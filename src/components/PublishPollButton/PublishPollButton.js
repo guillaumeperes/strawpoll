663a0918f20f33bc997a8dff7684c750a878be6c
Guillaume Peres <guillaumeperes10@gmail.com>
2017-05-12 13:21:43 +0200

    Publishpoll component

---
+++ src/components/PublishPollButton/PublishPollButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Component } from "react";
+import { Button } from "semantic-ui-react";
+import "./PublishPollButton.css";
+
+export default class PublishPollButton extends Component {
+	render() {
+		return (
+			<Button size="huge">{this.props.children}</Button>
+		);
+	}
+}

56568d17c8a70d8ee5e0284747f9520b4ce629e8
Guillaume Peres <guillaumeperes10@gmail.com>
2017-05-12 13:20:23 +0200

    QuestionInput component

---
+++ src/components/QuestionInput/QuestionInput.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Component } from "react";
+import { Form } from "semantic-ui-react";
+import { Input } from "semantic-ui-react";
+import { Icon } from "semantic-ui-react";
+import "./QuestionInput.css";
+
+export default class QuestionInput extends Component {
+	render() {
+		return (
+			<Form.Field>
+				<Input size="huge" focus id={this.props.id} type="text" placeholder={this.props.placeholder}></Input>
+			</Form.Field>
+		);
+	}
+}

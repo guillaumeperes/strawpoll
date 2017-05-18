db105bf715c76b37013a883a6b167e711cefda22
Guillaume Peres <guillaumeperes10@gmail.com>
2017-05-18 19:09:48 +0200

    page not found component

---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Component } from "react";
+import { Segment } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
+import { Icon } from "semantic-ui-react";
+import { Divider } from "semantic-ui-react";
+import { Header } from "semantic-ui-react";
+import { Button } from "semantic-ui-react";
+import AppTitle from "../AppTitle/AppTitle";
+import "./PageNotFound.css";
+
+export default class PageNotFound extends Component {
+	constructor(props) {
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick(event) {
+		event.preventDefault();
+		this.props.history.push("/");
+	}
+
+	render() {
+		return (
+			<Segment vertical id="PageNotFound" textAlign="center">
+				<Container text>
+					<AppTitle></AppTitle>
+					<Divider horizontal inverted></Divider>
+					<Icon inverted name="map outline" size="huge"></Icon>
+					<Header as="h2" inverted>Erreur, cette page est introuvable</Header>
+					<Divider horizontal inverted></Divider>
+					<Button primary size="large" onClick={this.handleClick}><Icon name="home"></Icon> Revenir à la page d'accueil</Button>
+				</Container>
+			</Segment>
+		);
+	}
+}

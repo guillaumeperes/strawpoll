e7ed0b9d11073facc2a8fc4ad30aa8c68fa5fb25
Guillaume Peres <guillaumeperes10@gmail.com>
2017-06-19 22:47:43 +0200

    améliore modal de partage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,9 @@ import uniqid from "uniqid";
 import PropTypes from "prop-types";
 import Router from "../Router/Router";
 import Footer from "../Footer/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+import "./App.css";
 
 class App extends Component {
 	static propTypes = {
@@ -30,7 +33,8 @@ class App extends Component {
 
 	render() {
 		return (
-			<div>
+			<div id="App">
+				<ToastContainer autoClose={4000} />
 				<Router />
 				<Footer />
 			</div>
--- src/components/ShareModal/ShareModal.js
+++ src/components/ShareModal/ShareModal.js
@@ -4,14 +4,21 @@ import { Button } from "semantic-ui-react";
 import { Modal } from "semantic-ui-react";
 import { Icon } from "semantic-ui-react";
 import { Input } from "semantic-ui-react";
+import { Header } from "semantic-ui-react";
 import copy from "copy-to-clipboard";
+import { toast } from "react-toastify";
 import "./ShareModal.css";
 
 export default class ShareModal extends Component {
 	constructor(props) {
 		super(props);
 		this.shareBasePath = "https://strawpoll.guillaumeperes.fr/poll/:poll_id/";
+		this.handleOpen = this.handleOpen.bind(this);
+		this.handleClose = this.handleClose.bind(this);
 		this.handleClipboardClick = this.handleClipboardClick.bind(this);
+		this.state = {
+			"opened": false
+		};
 	}
 
 	sharePath() {
@@ -21,18 +28,42 @@ export default class ShareModal extends Component {
 		return false;
 	}
 
+	handleOpen(event) {
+		event.preventDefault();
+		this.setState({
+			"opened": true
+		});
+	}
+
+	handleClose(event) {
+		event.preventDefault();
+		toast.dismiss(); // Ferme tous les pop-up à la sortie du modal
+		this.setState({
+			"opened": false
+		});
+	}
+
 	handleClipboardClick(event) {
 		event.preventDefault();
-		copy(this.sharePath());
+		toast.dismiss();
+		if (copy(this.sharePath())) {
+			toast.success("L'URL du sondage a été copiée dans votre presse-papier");
+		} else {
+			toast.error("Une erreur s'est produite");
+		}
 	}
 
 	render() {
 		return (
-			<Modal size="small" trigger={<Button fluid={this.props.triggerFluid} size={this.props.triggerSize} data-tooltip={this.props.triggerTooltip}>{this.props.children}</Button>}>
+			<Modal size="small" open={this.state.opened} trigger={<Button onClick={this.handleOpen} fluid={this.props.triggerFluid} size={this.props.triggerSize} data-tooltip={this.props.triggerTooltip}>{this.props.children}</Button>}>
 				<Modal.Header><Icon name="share alternate"></Icon> Partager ce sondage</Modal.Header>
 				<Modal.Content>
-					<Input fluid type="text" defaultValue={this.sharePath()} action={<Button data-tooltip="Copier dans le presse-papier" onClick={this.handleClipboardClick}><Icon name="clipboard"></Icon></Button>}></Input>
+					<Header as="h4">URL du sondage</Header>
+					<Input fluid focus type="text" defaultValue={this.sharePath()} action={<Button data-tooltip="Copier dans le presse-papier" onClick={this.handleClipboardClick}><Icon name="clipboard"></Icon></Button>}></Input>
 				</Modal.Content>
+				<Modal.Actions>
+					<Button primary onClick={this.handleClose}>Fermer</Button>
+				</Modal.Actions>
 			</Modal>
 		);
 	}

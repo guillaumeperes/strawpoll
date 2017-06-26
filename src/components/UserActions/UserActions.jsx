1bce4fe99b59d1599ed25702f3a32861bb859d41
Guillaume Peres <guillaumeperes10@gmail.com>
2017-06-26 23:38:41 +0200

    lien vers la liste des sondages

--- src/components/UserActions/UserActions.jsx
+++ src/components/UserActions/UserActions.jsx
@@ -3,6 +3,7 @@ import { Component } from "react";
 import { Dropdown } from "semantic-ui-react";
 import { connect } from "react-redux";
 import { withCookies } from "react-cookie";
+import { withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { toast } from "react-toastify";
 import axios from "axios";
@@ -10,13 +11,18 @@ import { removeUserToken } from "../../actions.js";
 
 class UserActions extends Component {
 	static propTypes = {
+		"match": PropTypes.object.isRequired,
+		"location": PropTypes.object.isRequired,
+		"history": PropTypes.object.isRequired,
 		"cookies": PropTypes.object.isRequired,
 	};
 
 	constructor(props) {
 		super(props);
 		this.handleLogout = this.handleLogout.bind(this);
+		this.handleUserPolls = this.handleUserPolls.bind(this);
 		this.userInfoUrl = "https://api.strawpoll.guillaumeperes.fr/api/user/:token/infos/";
+		this.userPollsPath = "/user/polls/";
 		this.state = {
 			"user": {}
 		};
@@ -56,6 +62,19 @@ class UserActions extends Component {
 		});
 	}
 
+	handleUserPolls(event) {
+		// Vérifie si le token existe encore et est valide
+		if (typeof(this.props.cookies.get("strawpoll_userToken")) === "undefined") {
+			this.props.removeUserTokenInStore();
+			toast.error("Une erreur inconnue est survenue, vous avez été déconnecté");
+			return;
+		}
+		// Redirige vers la liste des sondages
+		if (this.props.location.pathname !== "/user/polls/") {
+			this.props.history.push("/user/polls/");
+		}
+	}
+
 	handleLogout(event) {
 		this.props.cookies.remove("strawpoll_userToken");
 		this.props.removeUserTokenInStore();
@@ -66,7 +85,9 @@ class UserActions extends Component {
 		return (
 			<Dropdown text={this.state.user.email} icon="user" button labeled className="icon">
 				<Dropdown.Menu>
-					<Dropdown.Item onClick={this.handleLogout}>Deconnexion</Dropdown.Item>
+					<Dropdown.Item onClick={this.handleUserPolls}>Mes sondages</Dropdown.Item>
+					<Dropdown.Divider />
+					<Dropdown.Item onClick={this.handleLogout}>Déconnexion</Dropdown.Item>
 				</Dropdown.Menu>
 			</Dropdown>
 		);
@@ -87,4 +108,5 @@ const mapDispatchToProps = function(dispatch) {
 
 UserActions = connect(mapStateToProps, mapDispatchToProps)(UserActions);
 UserActions = withCookies(UserActions);
+UserActions = withRouter(UserActions);
 export default UserActions;
